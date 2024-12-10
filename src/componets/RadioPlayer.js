a5ebe397ba00b5aca9b03a66f23926b88ed609eb
rafaeru-png <rafaeru.png@gmail.com>
2024-12-10 20:55:15 -0300

    ver 0.03

--- src/componets/RadioPlayer.js
+++ src/componets/RadioPlayer.js
@@ -5,7 +5,7 @@ const RadioPlayer = () => {
         <div id="radio" className="text-center my-5">
             <h2>Ouça a Rádio</h2>
             <audio controls>
-                <source src="https://exemplo.com/streaming-url" type="audio/mpeg" />
+                <source src="https://8630.brasilstream.com.br/stream.m3u" type="audio/mpeg" />
                 Seu navegador não suporta o player de áudio.
             </audio>
         </div>
